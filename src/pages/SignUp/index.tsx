f51fd0c5c0380024c49f7c0d8cde1c39b1e09ac6
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-21 16:50:00 +0000

    base setup of unform library

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Registo</h1>
-        <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
-        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Palavra-Passe"
-        />
-        <Button type="submit">Registar</Button>
-        <a href="forgot">Recuperar senha</a>
-      </form>
-      <a href="login">
-        <FiArrowLeft />
-        Voltar
-      </a>
-    </Content>
-  </Container>
-);
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form initialData={{ name: 'Wolf' }} onSubmit={handleSubmit}>
+          <h1>Registo</h1>
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Palavra-Passe"
+          />
+          <Button type="submit">Registar</Button>
+          <a href="forgot">Recuperar senha</a>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
