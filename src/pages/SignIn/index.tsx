873399ef28350de42f8e0f4fdd2281e32c0842f3
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-29 17:47:34 +0000

    added sign in and register routes (with animations)

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,9 +3,11 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import { Link } from 'react-router-dom';
+
 import { useAuth } from '../../hooks/AuthContext';
 import { useToast } from '../../hooks/ToastContext';
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -47,6 +49,8 @@ const SignIn: React.FC = () => {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
+
+          return;
         }
 
         addToast({
@@ -62,30 +66,32 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Dados de acesso</h1>
-          <Input
-            name="email"
-            icon={FiMail}
-            type="text"
-            placeholder="E-mail"
-            autoComplete="username"
-          />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Palavra-Passe"
-            autoComplete="current-password"
-          />
-          <Button type="submit">Entrar</Button>
-          <a href="forgot">Recuperar senha</a>
-        </Form>
-        <a href="login">
-          <FiLogIn />
-          Criar conta
-        </a>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Dados de acesso</h1>
+            <Input
+              name="email"
+              icon={FiMail}
+              type="text"
+              placeholder="E-mail"
+              autoComplete="username"
+            />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Palavra-Passe"
+              autoComplete="current-password"
+            />
+            <Button type="submit">Entrar</Button>
+            <a href="forgot">Recuperar senha</a>
+          </Form>
+          <Link to="/register">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
       </Content>
       <Background />
     </Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,7 +3,9 @@ import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Container, Content, Background } from './styles';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -28,7 +30,7 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      // console.log(err);
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }
@@ -38,24 +40,26 @@ const SignUp: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Registo</h1>
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Palavra-Passe"
-          />
-          <Button type="submit">Registar</Button>
-          <a href="forgot">Recuperar senha</a>
-        </Form>
-        <a href="login">
-          <FiArrowLeft />
-          Voltar
-        </a>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Registo</h1>
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Palavra-Passe"
+            />
+            <Button type="submit">Registar</Button>
+            <a href="forgot">Recuperar senha</a>
+          </Form>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
