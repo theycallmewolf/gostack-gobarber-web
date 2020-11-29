1eba1ba740ec034559692b716a36bc824c45491c
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-29 14:16:02 +0000

    separated Toast component from ToastContainer

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,32 +1,17 @@
-import React, { useCallback } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { Container, Toast } from './styles';
-import { ToastMessage, useToast } from '../../hooks/ToastContext';
+import React from 'react';
+import { Container } from './styles';
+import { ToastMessage } from '../../hooks/ToastContext';
+import Toast from './Toast';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
   return (
     <Container>
       {messages.map(message => (
-        <Toast
-          hasDescription={!!message.description}
-          type={message.type}
-          key={message.id}
-        >
-          <FiAlertCircle size={20} />
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-          <button onClick={() => removeToast(message.id)} type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
