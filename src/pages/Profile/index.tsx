851c953ede17b45873772cbda44ca4f141e81fa8
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-16 11:52:52 +0000

    feat: adds profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,126 @@
+import React, { useCallback, useRef } from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft, FiCamera } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+import { Container, Content, AvatarInput } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+import { useToast } from '../../hooks/ToastContext';
+import { useAuth } from '../../hooks/AuthContext';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const { user } = useAuth();
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Adicione um email válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        history.push('/');
+        addToast({
+          type: 'success',
+          title: 'registo efetuado',
+          description: 'Já pode entrar no GoBarber',
+        });
+      } catch (err) {
+        // console.log(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'erro no registo',
+          description:
+            'ocorreu um erro ao enviar o seu registo. tente novamente',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+          onSubmit={handleSubmit}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <button type="button">
+              <FiCamera />
+            </button>
+          </AvatarInput>
+          <h1>O meu perfil</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            name="old_password"
+            icon={FiLock}
+            type="password"
+            placeholder="Palavra-passe atual"
+          />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Nova palavra-passe"
+          />
+          <Input
+            name="password_confirm"
+            icon={FiLock}
+            type="password"
+            placeholder="Confirmar palavra-passe"
+          />
+          <Button type="submit">Alterar Dados</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,133 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  > header {
+    height: 144px;
+    background: #28262e;
+
+    display: flex;
+    align-items: center;
+
+    div {
+      width: 100%;
+      max-width: 1120px;
+      margin: 0 auto;
+
+      svg {
+        color: #999591;
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /*  place-content: center; substitui as tags anteriores */
+
+  width: 100%;
+  margin: -176px auto 0;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+      font-size: 20px;
+      text-align: left;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 200ms ease-in-out;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 200ms ease-in-out;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+const appearFromRight = keyframes`
+  from { opacity: 0; transform: translateX(50px)}
+  to { opacity: 1; transform: translateX(0)}
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
+`;
+
+export const AvatarInput = styled.div`
+  position: relative;
+  width: 186px;
+  margin: 0 auto 32px;
+
+  img {
+    width: 186px;
+    height: 186px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+
+  button {
+    position: absolute;
+    width: 48px;
+    height: 48px;
+    background: #ff9000;
+    border: none;
+    border-radius: 50%;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: background 200ms ease-in-out;
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: #312e38;
+    }
+
+    &:hover {
+      background: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
