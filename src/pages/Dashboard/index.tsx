9b3f42f71778becfa4be9c413d47d76877a99c59
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-16 10:41:12 +0000

    adds link to Profile page on Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { isToday, format, parseISO, isAfter } from 'date-fns';
 import { pt } from 'date-fns/esm/locale';
 import DayPicker, { DayModifiers } from 'react-day-picker';
@@ -136,7 +137,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
