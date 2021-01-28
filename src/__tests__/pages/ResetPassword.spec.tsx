258fd0b828bc9f508876be96bb036a9e34aa931d
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-28 20:05:15 +0000

    adds reset password unity test

--- src/__tests__/pages/ResetPassword.spec.tsx
+++ src/__tests__/pages/ResetPassword.spec.tsx
@@ -6,28 +6,30 @@ const mockedHistoryPush = jest.fn();
 const mockedAddToast = jest.fn();
 const mockedApiResponse = jest.fn();
 
+jest.mock('../../services/api', () => ({
+  post() {
+    return {
+      response: mockedApiResponse,
+    };
+  },
+}));
+
 jest.mock('react-router-dom', () => ({
+  useLocation() {
+    return {
+      search: 'token=mocked-token',
+    };
+  },
   useHistory: () => ({
     push: jest.fn(mockedHistoryPush),
   }),
-  useLocation: () => ({
-    search: () => ({
-      replace: () => 'token',
-    }),
+}));
+
+jest.mock('../../hooks/ToastContext', () => ({
+  useToast: () => ({
+    addToast: mockedAddToast,
   }),
 }));
-
-jest.mock('../../services/api', () => ({
-  post: () => mockedApiResponse,
-}));
-
-jest.mock('../../hooks/ToastContext', () => {
-  return {
-    useToast: () => ({
-      addToast: mockedAddToast,
-    }),
-  };
-});
 
 describe('Reset Password page', () => {
   it('should be able to recover the password', async () => {
@@ -47,6 +49,26 @@ describe('Reset Password page', () => {
 
     await waitFor(() => {
       expect(mockedHistoryPush).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it("shouldn't be able to recover the password if inserted password don't match", async () => {
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova Password');
+    const passwordConfirmField = getByPlaceholderText('Confirmar Password');
+    const buttonElement = getByText('Recuperar');
+
+    fireEvent.change(passwordField, {
+      target: { value: 'fancy-new-password' },
+    });
+    fireEvent.change(passwordConfirmField, {
+      target: { value: 'different-password' },
+    });
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
     });
   });
 });
