84b9b2ff34f5adae42d404c0d7fd8365e677e1b8
bruno-wolf <bruno.m.lobato@gmail.com>
2021-01-28 23:48:47 +0000

    adds sign in unity test for auth hook

---
+++ src/__tests__/hooks/auth.spec.tsx
@@ -0,0 +1,43 @@
+import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
+import { AuthProvider, useAuth } from '../../hooks/AuthContext';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
+describe('Auth hook', () => {
+  it('should be able to sign in', async () => {
+    const apiResponse = {
+      user: {
+        id: 'userID',
+        name: 'dummy-name',
+        email: 'dummy@theycallmewolf.com',
+      },
+      token: 'dummy-token',
+    };
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'dummy@theycallmewolf.com',
+      password: 'dummy-password',
+    });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:token',
+      apiResponse.token,
+    );
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(apiResponse.user),
+    );
+    expect(result.current.user.email).toEqual('dummy@theycallmewolf.com');
+  });
+});
